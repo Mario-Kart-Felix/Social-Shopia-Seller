242a1862ddc29c14b6d7f27df255b68b249a4619
Social Codia <socialcodia@gmail.com>
2020-05-02 23:29:33 +0530

    Updated Constants Class

--- app/src/main/java/com/socialcodia/social_shopia/storage/Constants.java
+++ app/src/main/java/com/socialcodia/social_shopia/storage/Constants.java
@@ -1,4 +1,29 @@
 package com.socialcodia.social_shopia.storage;
 
 public class Constants {
-}
+        public static String
+                USER_MOBILE = "mobile",
+                USER_ID = "uid",
+                USER_IMAGE = "image",
+                USER_EMAIL = "email",
+                USER_NAME = "name",
+                CITY = "city",
+                COUNTRY ="county",
+                LATITUDE = "latitude",
+                LONGITUDE = "longitude",
+                STATE = "state",
+                TIMESTAMP ="timestamp",
+                USERS = "users",
+                ADDRESS = "address",
+                SHOP_NAME = "shopName",
+                DELIVERY_FEES = "deliveryFees"
+
+
+
+
+
+
+
+                        ;
+
+    }
