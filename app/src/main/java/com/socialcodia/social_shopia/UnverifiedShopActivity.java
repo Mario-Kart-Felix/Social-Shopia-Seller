fec10d81d9dd7763d48cea9016413e95b353b143
Social Codia <socialcodia@gmail.com>
2020-05-02 23:17:04 +0530

    Added UnverifiedShopActivity .

---
+++ app/src/main/java/com/socialcodia/social_shopia/UnverifiedShopActivity.java
@@ -0,0 +1,14 @@
+package com.socialcodia.social_shopia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UnverifiedShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unverified_shop);
+    }
+}
