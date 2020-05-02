66b21a056c2a82aa5b725e74605513ad9cdc4bc4
Social Codia <socialcodia@gmail.com>
2020-05-03 02:49:11 +0530

    Added EditShopActivity to edit the information of the seller's shop.

---
+++ app/src/main/java/com/socialcodia/social_shopia/EditShopActivity.java
@@ -0,0 +1,14 @@
+package com.socialcodia.social_shopia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_shop);
+    }
+}
