1d04592e9b34e3ba69ae5293e3e6806eacc336c6
Social Codia <socialcodia@gmail.com>
2020-05-03 02:58:39 +0530

    Added Splash Activity.

---
+++ app/src/main/java/com/socialcodia/social_shopia/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.socialcodia.social_shopia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
